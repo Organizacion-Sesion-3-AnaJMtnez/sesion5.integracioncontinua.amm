0757830cdf9e5f20848e9beba25480a6c69fa116
Ana Martínez <anamartinez.informatica@iespacomolla.es>
2023-04-16 18:49:14 +0200

    1ra version ispar

---
+++ ispar.js
@@ -0,0 +1,14 @@
+/* Función ispar */
+var par = function espar (a) {
+	return a;	
+};
+
+/* Función resta 
+var resta = function suma (a, b) {
+	return a - b;	
+}; */
+
+/* Exportación de funciones */
+module.exports = {
+	espar
+}
